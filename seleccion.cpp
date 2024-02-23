190da42d15b0c31a79a5d68d74fa6892ac2bfc24
UsiasSG <160407522+UsiasSG@users.noreply.github.com>
2024-02-23 15:00:10 -0600

    Add files via upload

---
+++ seleccion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+main(){
+	int n1=0,n2=0,n3=0,promedio=0;
+	cout<<"Ingrese Nota 1:";
+	cin>>n1;
+	cout<<"Ingrese Nota 2:";
+	cin>>n2;
+	cout<<"Ingrese Nota 3:";
+	cin>>n3;
+	promedio = (n1+n2+n3) / 3;
+	string mensaje="";
+	mensaje = promedio>=61 ? "Aprobo" : "Reprobo";
+	cout<<"Su promedio es de "<<promedio<<" por lo tanto "<<mensaje<<endl;
+
+    if(promedio>=0 && promedio<61){
+    	cout<<"Mala nota"<<endl;
+	}
+	if(promedio>=61 && promedio<81){
+		cout<<"Buena nota"<<endl;
+	}
+	if(promedio>=81){
+		cout<<"Excelente nota"<<endl;
+	}
+	
+	system("pause");
+	
+}
